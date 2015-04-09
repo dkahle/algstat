787595cfe03dc0397eaca249ede403c21ee2f594
David Kahle <david.kahle@gmail.com>
2015-04-09 00:28:09 -0500

    lawrence: docs and intDiag -> integerIdentityMatrix

--- R/lawrence.R
+++ R/lawrence.R
@@ -3,30 +3,47 @@
 #' Compute the Lawrence lifting of a configuration matrix
 #' 
 #' @param A the configuration matrix of the model
+#' @param extent short or tall version
 #' @return an integer matrix
 #' @author David Kahle \email{david.kahle@@gmail.com}
-#' @seealso Aoki, Hara, and Takemura (2012), p. 61
+#' @seealso Aoki, Hara, and Takemura (2012), pp. 61-62
 #' @export 
 #' @examples
 #' 
 #' 
 #' A <- hmat(c(2,2), 1:2)
 #' lawrence(A)
+#' lawrence(A, extent = "tall")
+#' 
+#' markov(A)
+#' markov(lawrence(A))
+#' markov(lawrence(A, extent = "tall"))
 #' 
 #' 
-lawrence <- function(A){
-  m <- nrow(A); n <- ncol(A)
-  mat <- matrix(0L, nrow = 2*m+n, ncol = 2*n)
-  mat[1:m,1:n] <- A
-  mat[(m+1):(2*m),(n+1):(2*n)] <- A
-  mat[(2*m+1):(2*m+n),1:n] <- intDiag(n)
-  mat[(2*m+1):(2*m+n),(n+1):(2*n)] <- intDiag(n)
+lawrence <- function(A, extent = c("short", "tall")){
+  
+  extent <- match.arg(extent)
+  m <- nrow(A); n <- ncol(A)  
+  
+  if(extent == "short"){
+    mat <- matrix(0L, nrow = m+n, ncol = 2*n)
+    mat[1:m,1:n] <- A
+    mat[(m+1):(m+n),1:n]         <- integerIdentityMatrix(n)
+    mat[(m+1):(m+n),(n+1):(2*n)] <- integerIdentityMatrix(n)  
+  } else if(extent == "tall"){
+    mat <- matrix(0L, nrow = 2*m+n, ncol = 2*n)
+    mat[1:m,1:n] <- A
+    mat[(m+1):(2*m),(n+1):(2*n)] <- A
+    mat[(2*m+1):(2*m+n),1:n]         <- integerIdentityMatrix(n)
+    mat[(2*m+1):(2*m+n),(n+1):(2*n)] <- integerIdentityMatrix(n)  
+  }
+  
   mat
 }
 
 
 
-intDiag <- function(int){
+integerIdentityMatrix <- function(int){
   mat <- as.integer(diag(int))
   dim(mat) <- c(int, int)
   mat
