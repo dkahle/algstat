d420ede761590412ae2191e113fd843f5e32c804
David Kahle <david.kahle@gmail.com>
2015-08-08 17:06:55 +0900

    zzz.R: ' -> "

--- R/zzz.r
+++ R/zzz.r
@@ -2,24 +2,24 @@
   
   if(is.mac()){ ## find the path on a mac	
   	
-    apps <- system('ls /Applications/', intern = TRUE)
+    apps <- system("ls /Applications/", intern = TRUE)
     lowerCaseApps <- tolower(apps)
-    home <- system('ls ~/', intern = TRUE)
+    home <- system("ls ~/", intern = TRUE)
     lowerHome <- tolower(home)
     
     # find Macaulay2
-    ndxApps  <- which(str_detect(lowerCaseApps, 'macaulay2'))
-    ndxHome  <- which(str_detect(lowerHome, 'macaulay2'))   
+    ndxApps  <- which(str_detect(lowerCaseApps, "macaulay2"))
+    ndxHome  <- which(str_detect(lowerHome, "macaulay2"))   
      
     if(length(ndxApps) == 0 && length(ndxHome) == 0){
       options(m2Path = NULL)
     } else if(length(ndxApps) != 0){
       macaulayDirFull <- apps[ndxApps]
-      path <- paste('/Applications/', macaulayDirFull, '/bin', sep = '')
+      path <- paste("/Applications/", macaulayDirFull, "/bin", sep = "")
       options(m2Path = path)
     } else if(length(ndxHome) != 0){
       macaulayDirFull <- home[ndxHome]
-      path <- paste('~/', macaulayDirFull, '/bin', sep = '')
+      path <- paste("~/", macaulayDirFull, "/bin", sep = "")
       options(m2Path = path)      
     }
     
@@ -27,18 +27,18 @@
     
     
     # find Bertini
-    ndxApps  <- which(str_detect(lowerCaseApps, 'bertini'))
-    ndxHome  <- which(str_detect(lowerHome, 'bertini'))   
+    ndxApps  <- which(str_detect(lowerCaseApps, "bertini"))
+    ndxHome  <- which(str_detect(lowerHome, "bertini"))   
         
     if(length(ndxApps) == 0 && length(ndxHome) == 0){
       options(bertiniPath = NULL)
     } else if(length(ndxApps) != 0){
       bertiniDirFull <- apps[ndxApps]
-      path <- paste('/Applications/', bertiniDirFull, sep = '')
+      path <- paste("/Applications/", bertiniDirFull, sep = "")
       options(bertiniPath = path)
     } else if(length(ndxHome) != 0){
       bertiniDirFull <- home[ndxHome]
-      path <- paste('~/', bertiniDirFull, sep = '')
+      path <- paste("~/", bertiniDirFull, sep = "")
       options(bertiniPath = path)    	
     }  
         
@@ -46,23 +46,23 @@
     
     
     # find LattE-integrale 
-    ndxApps  <- which(str_detect(lowerCaseApps, 'latte'))
-    ndxHome  <- which(str_detect(lowerHome, 'latte'))   
+    ndxApps  <- which(str_detect(lowerCaseApps, "latte"))
+    ndxHome  <- which(str_detect(lowerHome, "latte"))   
     
     if(length(ndxApps) == 0 && length(ndxHome) == 0){
       options(lattePath = NULL)
       packageStartupMessage(paste(
-        'LattE not found. ',
-        'Set the location with setLattePath().'
+        "LattE not found. ",
+        "Set the location with setLattePath()."
       ))
     } else if(length(ndxApps) != 0){
       latteDirFull <- apps[ndxApps]
-      path <- paste('/Applications/', latteDirFull, '/bin', sep = '')
+      path <- paste("/Applications/", latteDirFull, "/bin", sep = "")
       options(lattePath = path)
       options(markovPath = path)      
     } else if(length(ndxHome) != 0){
       latteDirFull <- home[ndxHome]
-      path <- paste('~/', latteDirFull, '/bin', sep = '')
+      path <- paste("~/", latteDirFull, "/bin", sep = "")
       options(lattePath = path)   
       options(markovPath = path)       
     }
@@ -88,9 +88,9 @@
     
     if(!whereis_is_accessible()){ # check for whereis, return if not found
       packageStartupMessage(paste(
-        'whereis not found. ',
-        'Try setting the paths with setM2Path(), setBertiniPath(), setLattePath(),',
-        'and setMarkovPath().'
+        "whereis not found. ",
+        "Try setting the paths with setM2Path(), setBertiniPath(), setLattePath(),",
+        "and setMarkovPath()."
       ))
       return()
     }
@@ -102,44 +102,44 @@
     }
     
     x <- whereis("m2")
-    if(str_detect(x, '/')){
+    if(str_detect(x, "/")){
       options(m2Path = dirname(x))
     } else {
       packageStartupMessage(paste(
-        'Macaulay2 not found. ',
-        'Set the location with setM2Path().'
+        "Macaulay2 not found. ",
+        "Set the location with setM2Path()."
       ))
     }   
     
     
   	x <- whereis("bertini")
-    if(str_detect(x, '/')){
+    if(str_detect(x, "/")){
       options(bertiniPath = dirname(x))
     } else {
       packageStartupMessage(paste(
-        'Bertini not found. ',
-        'Set the location with setBertiniPath().'
+        "Bertini not found. ",
+        "Set the location with setBertiniPath()."
       ))
     }
     
   	x <- whereis("count")
-    if(str_detect(x, '/')){
+    if(str_detect(x, "/")){
       options(lattePath = dirname(x))
     } else {
       packageStartupMessage(paste(
-        'LattE not found. ',
-        'Set the location with setLattePath().'
+        "LattE not found. ",
+        "Set the location with setLattePath()."
       ))
     }
     
 
   	x <- whereis("markov")
-    if(str_detect(x, '/')){
+    if(str_detect(x, "/")){
       options(markovPath = dirname(x))
     } else {
       packageStartupMessage(paste(
-        '4ti2 not found. ',
-        'Set the location with setMarkovPath().'
+        "4ti2 not found. ",
+        "Set the location with setMarkovPath()."
       ))
     }
     
@@ -152,16 +152,16 @@
     # note that this is done after os-x with an else
   	
     options(m2Path = NULL)
-    packageStartupMessage('Set Macaulay2 path with setM2Path().')    
+    packageStartupMessage("Set Macaulay2 path with setM2Path().")    
 
     options(bertiniPath = NULL)
-    packageStartupMessage('Set Bertini path with setBertiniPath().')        
+    packageStartupMessage("Set Bertini path with setBertiniPath().")        
     
     options(lattePath = NULL)
-    packageStartupMessage('Set LattE-integrale path with setLattePath().')            
+    packageStartupMessage("Set LattE-integrale path with setLattePath().")            
     
     options(markovPath = NULL)
-    packageStartupMessage('Set 4ti2 path with setMarkovPath().')                    
+    packageStartupMessage("Set 4ti2 path with setMarkovPath().")                    
     
   }
   
@@ -204,13 +204,13 @@ check_for_m2 <- function(){
 
   if(is.null(getOption("m2Path"))){
     packageStartupMessage(paste(
-      'Macaulay2 not found.',
-      'Set the location with setM2Path().'
+      "Macaulay2 not found.",
+      "Set the location with setM2Path()."
     ))
     return(invisible())
   }
   
-  if(length(list.files(getOption('m2Path'))) == 0){
+  if(length(list.files(getOption("m2Path"))) == 0){
     packageStartupMessage(paste(
       "Macaulay2 appears to be installed,",
       "but it's not where it was expected."
@@ -219,7 +219,7 @@ check_for_m2 <- function(){
     return(invisible())    
   }	
 	
-  if(!any('M2' == list.files(getOption('m2Path')))){
+  if(!any("M2" == list.files(getOption("m2Path")))){
     packageStartupMessage(paste(
       "Macaulay2 appears to be installed,",
       "but it's not where it was expected."
@@ -236,13 +236,13 @@ check_for_bertini <- function(){
 
   if(is.null(getOption("bertiniPath"))){
     packageStartupMessage(paste(
-      'Bertini not found.',
-      'Set the location with setBertiniPath().'
+      "Bertini not found.",
+      "Set the location with setBertiniPath()."
     ))
     return(invisible())
   }
   
-  if(length(list.files(getOption('bertiniPath'))) == 0){
+  if(length(list.files(getOption("bertiniPath"))) == 0){
     packageStartupMessage(paste(
       "Bertini appears to be installed,",
       "but it's not where it was expected."
@@ -251,7 +251,7 @@ check_for_bertini <- function(){
     return(invisible())    
   }	
 	
-  if(!any('bertini' == list.files(getOption('bertiniPath')))){
+  if(!any("bertini" == list.files(getOption("bertiniPath")))){
     packageStartupMessage(paste(
       "Bertini appears to be installed,",
       "but it's not where it was expected."
@@ -272,13 +272,13 @@ check_for_latte <- function(){
 
   if(is.null(getOption("lattePath"))){
     packageStartupMessage(paste(
-      'LattE not found.',
-      'Set the location with setLattePath().'
+      "LattE not found.",
+      "Set the location with setLattePath()."
     ))
     return(invisible())
   }
   
-  if(length(list.files(getOption('lattePath'))) == 0){
+  if(length(list.files(getOption("lattePath"))) == 0){
     packageStartupMessage(paste(
       "LattE appears to be installed,",
       "but it's not where it was expected."
@@ -287,7 +287,7 @@ check_for_latte <- function(){
     return(invisible())    
   }	
 	
-  if(!any('count' == list.files(getOption('lattePath')))){
+  if(!any("count" == list.files(getOption("lattePath")))){
     packageStartupMessage(paste(
       "LattE appears to be installed,",
       "but it's not where it was expected."
@@ -309,13 +309,13 @@ check_for_4ti2 <- function(){
 
   if(is.null(getOption("markovPath"))){
     packageStartupMessage(paste(
-      '4ti2 not found.',
-      'Set the location with setMarkovPath().'
+      "4ti2 not found.",
+      "Set the location with setMarkovPath()."
     ))
     return(invisible())
   }
   
-  if(length(list.files(getOption('markovPath'))) == 0){
+  if(length(list.files(getOption("markovPath"))) == 0){
     packageStartupMessage(paste(
       "4ti2 appears to be installed,",
       "but it's not where it was expected."
@@ -324,7 +324,7 @@ check_for_4ti2 <- function(){
     return(invisible())    
   }	
 	
-  if(!any('markov' == list.files(getOption('markovPath')))){
+  if(!any("markov" == list.files(getOption("markovPath")))){
     packageStartupMessage(paste(
       "4ti2 appears to be installed,",
       "but it's not where it was expected."
