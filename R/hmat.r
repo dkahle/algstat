9791d5ec080d6abf0d1c7a2e7416bd16003c1e25
David Kahle <david.kahle@gmail.com>
2016-05-11 15:11:40 -0500

    hmat: link to latter::genmodel

--- R/hmat.r
+++ R/hmat.r
@@ -9,6 +9,7 @@
 #'   variable
 #' @param facets the facets generating the hierarchical model, a
 #'   list of vectors of variable indices
+#' @seealso \code{\link{genmodel}}
 #' @return a named matrix
 #' @export hmat
 #' @references Drton, M., B. Sturmfels, and S. Sullivant (2009).
