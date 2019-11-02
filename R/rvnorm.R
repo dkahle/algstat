9252c147f76528962f719585df0c276d91739ed0
David Kahle <david.kahle@gmail.com>
2019-11-02 11:34:05 -0500

    1 poly: change normal_lpdf(0 | g, ndg*si) to normal_lpdf(0 | g/ndg, si)

--- R/rvnorm.R
+++ R/rvnorm.R
@@ -432,7 +432,7 @@ transformed parameters {
 } 
       
 model {
-  target += normal_lpdf(0.00 | g, ndg*si);
+  target += normal_lpdf(0.00 | g/ndg, si);
 }
       ",  .open = "{{", .close = "}}"
       )
